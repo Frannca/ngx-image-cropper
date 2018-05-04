2e1e6bf514c4758788bebdd2f1f383cdd6a99387
Renato França <renato.franca@frannca.com.br>
2018-05-04 11:45:14 -0400

    feat: First commit.

---
+++ src/lib/ngx-image-cropper.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-image-cropper',
+  template: `
+    <p>
+      ngx-image-cropper works!
+    </p>
+  `,
+  styles: []
+})
+export class NgxImageCropperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/lib/ngx-image-cropper.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { NgxImageCropperComponent } from './ngx-image-cropper.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [NgxImageCropperComponent],
+  exports: [NgxImageCropperComponent]
+})
+export class NgxImageCropperModule { }
