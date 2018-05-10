ae48d41e46ed3a8f43b6b3ddc5ae18e2f6623fa4
Renato França <renato.franca@frannca.com.br>
2018-05-10 17:41:23 -0400

    feat(angular): Add Angular to allow build the library.

--- src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngx-image-cropper
- */
-
-export * from './lib/ngx-image-cropper.service';
-export * from './lib/ngx-image-cropper.component';
-export * from './lib/ngx-image-cropper.module';
